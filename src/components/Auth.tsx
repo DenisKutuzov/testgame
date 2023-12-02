4f36c116b33b2763bacd239751252f802857ebc9
DenisKutuzov <Denis-kutuzofff@yandex.ru>
2023-12-02 17:03:07 +0600

    commit

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -6,7 +6,7 @@ import { Config, Connect } from '@vkontakte/superappkit'
 import styles from './Auth.module.scss'
 
 const Auth = () => {
-  const { data: session, status } = useSession()
+  // const { data: session, status } = useSession()
 
   Config.init({
     appId: 51806222, // Тут нужно подставить ID своего приложения.
@@ -30,13 +30,13 @@ const Auth = () => {
       screen: undefined,
     })
 
-  console.log(session)
+  // console.log(session)
 
   return (
     <div>
-      <button onClick={() => signIn('vk')}>VK</button>
-      <button onClick={() => signOut()}>Выйти</button>
-      {session && session.user?.name && <div>{session.user?.name}</div>}
+      {/* <button onClick={() => signIn('vk')}>VK</button>
+      <button onClick={() => signOut()}>Выйти</button> */}
+      {/* {session && session.user?.name && <div>{session.user?.name}</div>} */}
       {/* {session && <div>{session.user}</div>} */}
 
       <button className={styles.btn} onClick={redirectAuthHandler}>
